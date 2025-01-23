9492f89e6ffce6ec1418163c43d2101a81496972
tsaijay <piano80742@gmail.com>
2025-01-24 02:45:50 +0800

    check

--- web_server/server.py
+++ web_server/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import base64
-from ai_model.digit_classifier import DigitClassifierService
+# from ai_model.digit_classifier import DigitClassifierService
 
 app = Flask(__name__)
 CORS(app)  # This allows cross-origin requests
